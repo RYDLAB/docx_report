1031444b81408d99d1881ce79a988c6e72cc8600
Stepan Savelyev <stepan.savelyev@rydlab.ru>
2020-01-14 11:30:22 +0500

    [ADD] msword icon view

--- __manifest__.py
+++ __manifest__.py
@@ -12,6 +12,7 @@
     "version": "0.1.1",
     "depends": ["base", "contacts", "russian_requisites", "sale",],
     "data": [
+        "data/assets_extension.xml",
         "data/fields_default.xml",
         "data/payment_terms.xml",
         "security/ir.model.access.csv",

94dd561e7f54daa895f03768cb40c10256d417c9
Stepan Savelyev <stepan.savelyev@rydlab.ru>
2020-01-20 16:27:34 +0500

    [IMP] add currency_id to view currency after-field

--- models/crm_lead.py
+++ models/crm_lead.py
@@ -4,4 +4,5 @@ from odoo import fields, models
 class CrmLead(models.Model):
     _inherit = "crm.lead"
 
-    cost_price = fields.Integer(string="Cost Price", default=0)
+    cost_price = fields.Monetary(string="Cost Price", default=0)
+    currency_id = fields.Many2one(related="company_id.currency_id", string="Currency", readonly=True)

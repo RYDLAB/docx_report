ea0158a8ef282a96943f6944f08b899cbb85f0b7
Stepan Savelyev <stepan.savelyev@rydlab.ru>
2020-02-26 17:31:04 +0500

    [UPD] Black reformat

--- utils/misc.py
+++ utils/misc.py
@@ -2,8 +2,7 @@ import datetime as dt
 import inspect
 
 from odoo import fields
-from odoo.tools.misc import (DEFAULT_SERVER_DATE_FORMAT,
-                             DEFAULT_SERVER_DATETIME_FORMAT)
+from odoo.tools.misc import DEFAULT_SERVER_DATE_FORMAT, DEFAULT_SERVER_DATETIME_FORMAT
 
 
 class IDocument(object):
